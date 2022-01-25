709b94acbacdd2888d5686e7e8600b27556f35e1
Dwdr <93760891+Dwdr@users.noreply.github.com>
2022-01-25 09:32:44 +0800

    Add files via upload

---
+++ Lab1Ex1.java
@@ -0,0 +1,5 @@
+class Lab1Ex1 {
+  public static void main(String[] args) {
+    System.out.println("Hello, I am Darren!");
+  }
+}
---
+++ Lab1Ex3.java
@@ -0,0 +1,42 @@
+class Lab1Ex3 {
+  public static void main(String[] args) {
+    String mod = "CSC1009";   
+        switch(mod) 
+        {  
+            case "CSC1001":   
+                System.out.println("Introduction to Computing");
+                break;
+            case "CSC1002":   
+                System.out.println("Mathematics 1");
+                break;
+            case "CSC1003":   
+                System.out.println("Programming Methodology");
+                break;
+            case "CSC1004":   
+                System.out.println("Computer Organisation and Architecture"); 
+                break;
+            case "CSC1005":   
+                System.out.println("Business Information Technology");
+                break;
+            case "CSC1006":   
+                System.out.println("Mathematics 2");
+                break;
+            case "CSC1007":   
+                System.out.println("Operating Systems");
+                break; 
+            case "CSC1008":   
+                System.out.println("Data Structures and Algorithms"); 
+                break;
+            case "CSC1009":   
+                System.out.println("Object-Oriented Programming");
+                break;
+            case "CSC1010":   
+                System.out.println("Computer Networks");
+                break;
+            default:    
+                System.out.println("Unknown Module"); 
+                break; 
+                
+  }
+  }
+}
